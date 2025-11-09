4a99eccd14fbc2fa5c57f6e60cc9b738859d8fae
naga251602 <gaurav.byte123+github@gmail.com>
2025-11-09 01:54:14 -0800

    add Length_of_last_word.py

---
+++ Length_of_last_word/length_of_last_word.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        s = s.strip()
+        
+        if len(s) == 1: return 1
+
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] != " ":
+                length += 1
+            elif length != 0: break
+        return length
