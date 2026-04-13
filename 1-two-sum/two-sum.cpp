27bc5e108f9050eab5fcc332eb71fee0e1a23012
GAURAV NAGALAPURAM VENKATARAMANAN <nagalapu@usc.edu>
2026-04-13 02:52:03 -0700

    Time: 4 ms (52.31%) | Memory: 14.9 MB (36.44%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> m;
+        int i;
 
-        for(int i = 0; i < nums.size(); i ++) {
-            m[target - nums[i]] = i;
-        }
-
-        int i = 0;
         for(i = 0; i < nums.size(); i ++) {
-            if (m.find(nums[i]) != m.end() && m[nums[i]] != i) break;
+            int k = target - nums[i];
+            if (m.find(nums[i]) != m.end()) return {m[nums[i]], i};
+            else m.insert({k, i});
         }
 
-        return {i, m[nums[i]]};
+        return {};
     }
 };
