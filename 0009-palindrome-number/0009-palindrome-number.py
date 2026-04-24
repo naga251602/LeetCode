cb6d0a7710589c0e173e8ffe17216c6da73d1e9f
GAURAV NAGALAPURAM VENKATARAMANAN <nagalapu@usc.edu>
2026-04-24 12:50:54 -0700

    0009-PALINDROME-NUMBER Stats: Time: 11 ms (37.11%), Space: 19.3 MB (57.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        u_l = (2 ** 31) - 1
+        if x >= 0:
+            rev = 0
+            while x > 0:
+                if rev >= u_l/10: return 0 
+                rev = rev * 10 + (x%10)
+                x //= 10
+            return rev
+        else:
+            return -self.reverse(-x)
+
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        return x == self.reverse(x)
+        
--- README.md
+++ README.md
@@ -15,6 +15,7 @@ A collection of LeetCode questions to ace the coding interview! - Created using 
 |  |
 | ------- |
 | [0007-reverse-integer](https://github.com/naga251602/LeetCode/tree/master/0007-reverse-integer) |
+| [0009-palindrome-number](https://github.com/naga251602/LeetCode/tree/master/0009-palindrome-number) |
 | [0050-powx-n](https://github.com/naga251602/LeetCode/tree/master/0050-powx-n) |
 ## Recursion
 |  |
