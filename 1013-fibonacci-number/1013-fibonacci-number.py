45c99feff4390b31851628a323f2b8bae97c6a2f
GAURAV NAGALAPURAM VENKATARAMANAN <nagalapu@usc.edu>
2026-04-30 10:53:50 -0700

    1013-FIBONACCI-NUMBER Stats: Time: 50 ms (49.58%), Space: 19.3 MB (19.55%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,5 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0: return 0
-        if n == 1: return 1
-        return self.fib(n-1) + self.fib(n-2)
+        m = defaultdict()
+        def f(n):
+            if n == 0: return 0
+            if n == 1: return 1
+            if m.get(n, -1) != -1: return m.get(n)
+            m[n] = f(n-1) + f(n-2)
+
+            return m[n]
+        return f(n)
